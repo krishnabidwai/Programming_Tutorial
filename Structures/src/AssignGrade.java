bc677247e61651a28113dbbf2a7ce94fb6165b4c
Juan Gonzalez Cavero <kice5823@in.uni-weimar.de>
2017-10-18 17:03:06 +0200

    first exercise done

--- Structures/src/AssignGrade.java
+++ Structures/src/AssignGrade.java
@@ -5,11 +5,24 @@ import java.util.Scanner;
 public class AssignGrade {
 	
 	public static void main(String[] args) {
+		
 		Scanner scan = new Scanner(System.in);
+		double input = scan.nextInt();
 		
-		//todo: when executing the program you will be ask to write a score.
-		//The program converts the score then into a grade and prints that grade (1.0, 1.3, 1.7, ...)
+		double grade = ((100 - input) / 50) * 3 + 1;
+		
+		if(grade >= 4) {
+ 
+			System.out.println("Failed - Best Luck next time. Grade ="+grade);
+			
+			
+		}else {
+			
+			System.out.println("Congrats Passed Grade ="+grade);
+		}
+		
+		//todo: when executing the program you will be ask to write a score. 
+		//The program converts the score then into a grade and prints that grade (1.0, 1.3, 1.7, ...) 
 		//Note that the student passed when the student gets a score of 50.0 points (4.0)
-		
 	}
 }
--- Structures/src/EvenOdd.java
+++ Structures/src/EvenOdd.java
@@ -5,7 +5,25 @@ import java.util.Scanner;
 public class EvenOdd {
 
 	public static void main(String[] args) {
+		
+		
 		Scanner scan = new Scanner(System.in);
+		
+		Integer x = scan.nextInt();
+		
+		for(int i = 1; i <= x; i++) {
+			
+			if(i%2 == 0) {
+				
+				System.out.println("Even Number "+i);
+				
+			}else {
+				
+				System.out.println("Odd Number "+i);
+			}
+		}
+		
+		
 		
 		//todo: The program takes a number as input.
 		//It then goes through all integer values till reaching that value and prints all the odd ones.
